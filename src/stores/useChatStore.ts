ce3382930d49c1a34e5daa25d7819691357bc0db
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-06-08 22:55:17 +0530

    create right sidebar in main layout

---
+++ src/stores/useChatStore.ts
@@ -0,0 +1,29 @@
+import { apiCaller } from "@/lib/axios";
+import { create } from "zustand";
+
+interface ChatStore {
+  users: any[];
+  isLoading: boolean;
+  error: string | null;
+
+  fetchUsers: () => Promise<void>;
+}
+
+export const useChatStore = create<ChatStore>((set) => ({
+  users: [],
+  isLoading: false,
+  error: null,
+
+  fetchUsers: async () => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await apiCaller.get("/users");
+      set({ users: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
