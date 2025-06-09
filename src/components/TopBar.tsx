1903b1984b5fa80fdca625da79b22e4695934582
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-06-09 06:46:15 +0530

    add logo forgot to save

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -11,7 +11,7 @@ const TopBar = () => {
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
       <div className="flex gap-2 items-center">
-        <img src="/logo.png" className="size-10" alt="Music Mate Logo" />
+        <img src="/logo.png" className="size-8" alt="Music Mate Logo" />
         Music Mate
       </div>
       <div className="flex items-center gap-4">
