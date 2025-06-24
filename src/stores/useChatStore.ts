2d9ba6b41946994ea80ac7227f2d1b5b4c2c5977
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-06-24 18:34:55 +0530

    ready for production

--- src/stores/useChatStore.ts
+++ src/stores/useChatStore.ts
@@ -22,7 +22,8 @@ interface ChatStore {
   setSelectedUser: (user: User | null) => void;
 }
 
-const baseUrl = "http://localhost:5000";
+const baseUrl =
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 const socket = io(baseUrl, {
   autoConnect: false,
   withCredentials: true,
